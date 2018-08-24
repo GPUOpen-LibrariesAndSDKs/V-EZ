ef6e35584d4d52894faaed533fb9a5bc37c5aea6
Christian Friedrich Coors <me@ccoors.de>
2018-08-24 08:35:45 +0200

    Fix compilation

--- Source/Core/Device.cpp
+++ Source/Core/Device.cpp
@@ -19,6 +19,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 //
+#include <cmath>
 #include <iostream>
 #include <array>
 #include <unordered_map>
